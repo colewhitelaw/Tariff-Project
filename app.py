05354bdb887d00c3b6b4ee6d1ebac671f9ff4d37
colewhitelaw <cwhitela@usc.edu>
2025-05-02 22:50:26 -0700

    render

--- app.py
+++ app.py
@@ -82,6 +82,8 @@ app.layout = html.Div([
     ], fluid=True)
 ])
 
-# Run the app
+# Create server variable for Gunicorn
+server = app.server
+
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050))) 
